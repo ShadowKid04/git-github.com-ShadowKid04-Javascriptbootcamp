0fa1a3ca0807ffe905f046c51d4ea27d4dde2b32
ShadowKid04 <iamenderboy04@gmail.com>
2021-12-19 17:47:31 +0100

    test

---
+++ story_time.js
@@ -0,0 +1,56 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let place = "Clock Tower";
+let city = "Astáthco";
+let shadow = "Raven";
+let kyani = "Cat";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let year = 2021;
+let time = 4;
+let tryes = 24;
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let items = ["Letters", "Wings", "Sticks"];
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let canFly = false;
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+
+console.log(`In the city of ${city} there was a ${place} in the year ${year}.`);
+console.log(`On top of the ${place} lived a ${kyani}.`);
+console.log(
+  `The ${kyani} lived there singing songs and entertaining the city of ${city}.`
+);
+console.log(
+  `However even tho the ${kyani} entertained the city of ${city}, she was all alone. ${"\n"}`
+);
+console.log(
+  `One day a flightless ${shadow}, walked passed the ${place} and saw the ${kyani}, and started talking to her.`
+);
+console.log(
+  `The ${shadow} and the ${kyani} would talk everyday, even into the late hours of ${time} in the morning. And they would even sending ${items[0]} to each others when the raven had to leave.`
+);
+console.log(
+  `And even tho the ${kyani} was stuck on top of the ${place} and the ${shadow} was stuck on the ground, they become extremly close. ${"\n"}`
+);
+console.log(
+  `One time the ${shadow} tried to make a ladder with a bunch of ${
+    items[2]
+  }, but there where not enough ${
+    items[2]
+  } in the city to build a latter to the top of the ${place}. ${"\n"}`
+);
+console.log(
+  `It came to the point where the flighless ${shadow} would try to fly up there, and yes it might be ${canFly} that he can fly, but he tried ${tryes} times.`
+);
+console.log(
+  `On his ${tryes}th try he pushed his ${
+    items[1]
+  } like he had never before and the flightless ${shadow} was able to fly. ${"\n"}`
+);
+console.log(
+  `${shadow} flew as high as he could..... unfortantely, it was not high enough, as when he nearly reach the top of the ${place}, his ${items[1]} failed him and he fell.`
+);
